7291219c91648b48d0fcb620a366512536f36442
Waqas Tahir <wakaztahir@gmail.com>
2021-07-11 15:52:48 +0500

    movie delete and edit

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"backend/models"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
+	"time"
 )
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -68,16 +76,94 @@ func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
-	//todo make insertMovie
+type MoviePayload struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Year        string `json:"year"`
+	ReleaseDate string `json:"release_date"`
+	Runtime     string `json:"runtime"`
+	Rating      string `json:"rating"`
+	MPAARating  string `json:"mpaa_rating"`
 }
 
-func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
-	//todo make updateMovie
+func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
+	var payload MoviePayload
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var movie models.Movie
+
+	if payload.ID != "0" {
+		id, _ := strconv.Atoi(payload.ID)
+		m, _ := app.models.DB.Get(id)
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
+	movie.ID, _ = strconv.Atoi(payload.ID)
+	movie.Title = payload.Title
+	movie.Description = payload.Description
+	movie.ReleaseDate, _ = time.Parse("2006-01-02", payload.ReleaseDate)
+	movie.Year = movie.ReleaseDate.Year()
+	movie.Runtime, _ = strconv.Atoi(payload.Runtime)
+	movie.Rating, _ = strconv.Atoi(payload.Rating)
+	movie.MPAARating = payload.MPAARating
+	movie.CreatedAt = time.Now()
+	movie.UpdatedAt = time.Now()
+
+	if movie.ID == 0 {
+		err = app.models.DB.InsertMovie(movie)
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
+	}
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	ok := jsonResponse{
+		OK: true,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, ok, "response")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
 }
 
 func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
-	//todo make deleteMovie
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id , err := strconv.Atoi(params.ByName("id"))
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.models.DB.DeleteMovie(id)
+
+	ok := jsonResponse{
+		OK: true,
+		Message: "Deleted",
+	}
+
+	err = app.writeJSON(w,http.StatusOK, ok , "response")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,5 +12,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres/", app.getAllGenres)
+	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
+
 	return app.enableCORS(router)
 }
